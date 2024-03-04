a3f150cb516341986bc8bb8209a4b9554bb8df8c
Greywolfie01 <yuravbeo@gmail.com>
2024-03-04 17:51:39 +0200

    Include bash

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Navigate to the Rust project directory
+cd Rust/
+
+# Run the Rust program
+cargo run
+
+# Return to main project directory
+cd ..
+
+# Navigate to C project directory
+
+# Run the C program
+
+# Return to main project directory
+cd ..
+
+# Execute the Python script for analysis
+python main.py
